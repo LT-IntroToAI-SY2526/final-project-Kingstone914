52df7057b032f22b5500ad37e7af10376fda0997
Kingston Yuen <kyuen@cps.edu>
2026-05-19 11:30:11 -0500

    prgoress

--- a11.py
+++ a11.py
@@ -153,6 +153,24 @@ def get_death_date(name: str) -> str:
 
     return match.group("death")
 
+def get_pokemon_director(wiki_text: str) -> str:
+    """Gets death date of the given person
+
+    Args:
+        name - name of the person
+
+    Returns:
+        death date of the given person
+    """
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(wiki_text)))
+    print(infobox_text)
+    pattern = r"(?:Died)(?:[\w \d,]*\()(?P<death>\d{4}-\d{2}-\d{2})"
+    error_text = (
+        "Page infobox has no director information (at least none in xxxx-xx-xx format)"
+    )
+    match = get_match(infobox_text, pattern, error_text)
+
+    return match.group("director")
 
 # below are a set of actions. Each takes a list argument and returns a list of answers
 # according to the action and the argument. It is important that each function returns a
@@ -192,6 +210,16 @@ def polar_radius(matches: List[str]) -> List[str]:
     """
     return [get_polar_radius(matches[0])]
 
+def pokemon_director(matches: List[str]) -> List[str]:
+    """Returns polar radius of planet in matches
+
+    Args:
+        matches - match from pattern of planet to find polar radius of
+
+    Returns:
+        polar radius of planet
+    """
+    return [get_pokemon_director(matches[0])]
 
 # dummy argument is ignored and doesn't matter
 def bye_action(dummy: List[str]) -> None:
@@ -209,6 +237,7 @@ pa_list: List[Tuple[Pattern, Action]] = [
     ("when was % born".split(), birth_date),
     ("when did % die".split(), death_date),
     ("what is the polar radius of %".split(), polar_radius),
+    ("who is the director of %".split(), pokemon_director),
     (["bye"], bye_action),
 ]
 
