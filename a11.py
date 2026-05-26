d3b7cf713f037a025216596ff90a29df25beb9ff
Kingston Yuen <kyuen@cps.edu>
2026-05-26 10:56:19 -0500

    finished

--- a11.py
+++ a11.py
@@ -267,6 +267,62 @@ def get_pokemon_genre(pokemon: str) -> str:
 
     return match.group("genre")
 
+def get_release_console_pokemon(wiki_text: str):
+    
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(wiki_text)))
+    #print(infobox_text)
+    pattern = r"(?:Platform)(?P<name>\w+ \w+)Release"
+    error_text = (
+        "Page infobox has no release game information"
+    )
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("name")
+
+def get_director_pokemon(wiki_text: str):
+    
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(wiki_text)))
+    # print(infobox_text)
+    pattern = r"(?:Director|Director |Directors)(?P<name>\w+ \w+)Producers"
+    error_text = (
+        "Page infobox has no release game information"
+    )
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("name")
+
+def get_artist_pokemon(wiki_text: str):
+    
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(wiki_text)))
+    # print(infobox_text)
+    pattern = r"(?:Artist)(?P<name>\w+ \w+)Writer"
+    error_text = (
+        "Page infobox has no artist information"
+    )
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("name")
+
+def get_writer_pokemon(wiki_text: str):
+    
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(wiki_text)))
+    # print(infobox_text)
+    pattern = r"(?:Writer)(?P<name>\w+ \w+)Composers"
+    error_text = (
+        "Page infobox has no writer information"
+    )
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("name")
+
+def get_programmer_pokemon(wiki_text: str):
+    
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(wiki_text)))
+    # print(infobox_text)
+    pattern = r"(?:Programmer|Programmers)(?P<name>\w+ \w+)Artists|Artist"
+    error_text = (
+        "Page infobox has no writer information"
+    )
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("name")
+
+
 # below are a set of actions. Each takes a list argument and returns a list of answers
 # according to the action and the argument. It is important that each function returns a
 # list of the answer(s) and not just the answer itself.
@@ -371,6 +427,37 @@ def pokemon_genre(matches: List[str]) -> List[str]:
     """
     return [get_pokemon_genre(matches[0])]
 
+def release_console_pokemon(matches: List[str]) -> List[str]:
+
+
+    
+    return [get_release_console_pokemon(matches[0])]
+
+def director_pokemon(matches: List[str]) -> List[str]:
+
+
+    
+    return [get_director_pokemon(matches[0])]
+
+def artist_pokemon(matches: List[str]) -> List[str]:
+
+
+    
+    return [get_artist_pokemon(matches[0])]
+
+def writer_pokemon(matches: List[str]) -> List[str]:
+
+
+    
+    return [get_writer_pokemon(matches[0])]
+
+def programmer_pokemon(matches: List[str]) -> List[str]:
+
+
+    
+    return [get_programmer_pokemon(matches[0])]
+
+
 # dummy argument is ignored and doesn't matter
 def bye_action(dummy: List[str]) -> None:
     raise KeyboardInterrupt
@@ -393,6 +480,11 @@ pa_list: List[Tuple[Pattern, Action]] = [
     ("when was % released".split(), pokemon_release),
     ("who is the publisher of %".split(), pokemon_publisher),
     ("what is the genre of %".split(), pokemon_genre),
+    ("what console was % released on".split(), release_console_pokemon),
+    ("who is the director of %".split(), director_pokemon),
+    ("who is the artist of %".split(), artist_pokemon),
+    ("who is the writer of %".split(), writer_pokemon),
+    ("who is the programmer of %".split(), programmer_pokemon),
     (["bye"], bye_action),
 ]
 
